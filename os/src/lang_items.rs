2807b203f677e18b5a1bae5a4a21c96680b0616a
logeable <logeable@gmail.com>
2022-11-29 15:12:47 +0000

    feat: sbi

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,5 +1,16 @@
 use core::panic::PanicInfo;
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "\x1b[31mPanicked at {}:{} {}\x1b[0m",
+            location.file(),
+            location.line(),
+            info.message().unwrap(),
+        );
+    } else {
+        println!("\x1b[31mPanicked: {}\x1b[0m", info.message().unwrap());
+    }
+
+    crate::sbi::shutdown()
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
 mod lang_items;
+mod sbi;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
@@ -8,7 +13,9 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    loop {}
+    let msg = "this is simple kernel";
+    println!("{}", msg);
+    panic!("shutdown!");
 }
 
 fn clear_bss() {
