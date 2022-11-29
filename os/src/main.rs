89823db07b22dbbaaeca9f8cbea1cdd2eb2c2551
logeable <logeable@gmail.com>
2022-11-29 15:52:22 +0000

    print kernel layout

--- os/src/main.rs
+++ os/src/main.rs
@@ -13,9 +13,9 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    let msg = "this is simple kernel";
-    println!("{}", msg);
-    panic!("shutdown!");
+    print_layout();
+
+    sbi::shutdown();
 }
 
 fn clear_bss() {
@@ -24,4 +24,30 @@ fn clear_bss() {
         fn ebss();
     }
     (sbss as usize..ebss as usize).for_each(|x| unsafe { (x as *mut u8).write_volatile(0) })
+}
+
+fn print_layout() {
+    extern "C" {
+        fn skernel();
+        fn stext();
+        fn etext();
+
+        fn sdata();
+        fn edata();
+
+        fn sstack();
+        fn estack();
+        fn sbss();
+        fn ebss();
+        fn ekernel();
+        //fn rust_main();
+    }
+    println!(
+        "kernel [{:#x}, {:#x}] rust_main = {:#x}",
+        skernel as usize, ekernel as usize, rust_main as usize
+    );
+    println!(".text [{:#x}, {:#x}]", stext as usize, etext as usize);
+    println!(".data [{:#x}, {:#x}]", sdata as usize, edata as usize);
+    println!("stack [{:#x}, {:#x}]", sstack as usize, estack as usize);
+    println!(".bss [{:#x}, {:#x}]", sbss as usize, ebss as usize);
 }
