277ed369ec969e6b5468269bf60b10b0672e7937
logeable <logeable@gmail.com>
2023-01-18 01:44:27 +0000

    feat: 支持动态内存分配

--- os/src/config.rs
+++ os/src/config.rs
@@ -4,3 +4,4 @@ pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const CLOCK_FREQ: usize = 12500000;
+pub const KERNEL_HEAP_SIZE: usize = 0x300000;
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
 #[macro_use]
 mod console;
 mod config;
 mod lang_items;
 mod loader;
 mod logger;
+mod mm;
 mod sbi;
 mod stack_trace;
 mod sync;
@@ -17,20 +20,23 @@ mod timer;
 mod trap;
 
 use core::arch::global_asm;
+
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
+    print_layout();
+
+    mm::init();
     logger::init().unwrap();
     trap::init();
     loader::load_apps();
-
-    print_layout();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
+
     unreachable!()
 }
 
